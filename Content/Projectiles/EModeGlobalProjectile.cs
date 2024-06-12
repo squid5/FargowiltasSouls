e1a0e241132f6e2a03b737ff9e9b15242630ce65
JavyzTaken <jacklukas@outlook.com>
2024-06-12 13:01:29 +0200

    Removed Maso WoF laser burning debuff

--- Content/Projectiles/EModeGlobalProjectile.cs
+++ Content/Projectiles/EModeGlobalProjectile.cs
@@ -1472,8 +1472,6 @@ namespace FargowiltasSouls.Content.Projectiles
                     if (sourceNPC != null && (sourceNPC.type == NPCID.WallofFlesh || sourceNPC.type == NPCID.WallofFleshEye))
                     {
                         target.AddBuff(BuffID.OnFire, 300);
-                        if (WorldSavingSystem.MasochistModeReal)
-                            target.AddBuff(BuffID.Burning, 60);
                     }
                     break;
 
