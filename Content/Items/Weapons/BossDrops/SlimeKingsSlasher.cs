ce1d3252e49da68ff0d4373d92452af28e061d64
JavyzTaken <jacklukas@outlook.com>
2024-08-07 20:52:14 +0200

    Slime Kings Slasher: Changed swing animation, deals +20% damage to enemies on fire

--- Content/Items/Weapons/BossDrops/SlimeKingsSlasher.cs
+++ Content/Items/Weapons/BossDrops/SlimeKingsSlasher.cs
@@ -15,6 +15,7 @@ namespace FargowiltasSouls.Content.Items.Weapons.BossDrops
 {
     public class SlimeKingsSlasher : SoulsItem
     {
+        int Timer = 0;
         public override void SetStaticDefaults()
         {
             Terraria.GameContent.Creative.CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
@@ -26,22 +27,59 @@ namespace FargowiltasSouls.Content.Items.Weapons.BossDrops
             Item.DamageType = DamageClass.Melee;
             Item.width = 40;
             Item.height = 44;
-            Item.useTime = 10;
-            Item.useAnimation = 20;
-            Item.reuseDelay = 30;
+            Item.useTime = 25;
+            Item.useAnimation = 25;
             Item.useStyle = ItemUseStyleID.Swing;
             Item.knockBack = 6;
             Item.value = 10000;
             Item.rare = ItemRarityID.Green;
-            Item.UseSound = SoundID.Item1;
+            //Item.UseSound = SoundID.Item1;
             Item.autoReuse = true;
             Item.scale = 1.5f;
         }
-        public override bool? UseItem(Player player)
+        public override void HoldItem(Player player) //fancy momentum swing, this should be generalized and applied to other swords imo
         {
-            if (player.itemTime == 0)
+            if (player.itemAnimation == 0)
+            {
+                Timer = 0;
+                return;
+            }
+
+            if (player.itemAnimation == player.itemAnimationMax)
+            {
+                Timer = player.itemAnimationMax;
+            }
+            if (player.itemAnimation > 0)
+            {
+                Timer--;
+            }
+            if (Timer == player.itemAnimationMax / 2)
+            {
                 SoundEngine.PlaySound(SoundID.Item1, player.Center);
-            return base.UseItem(player);
+                //SoundEngine.PlaySound(SoundID.Item39, player.Center);
+            }
+            if (Timer > 2 * player.itemAnimationMax / 3)
+            {
+                player.itemAnimation = player.itemAnimationMax;
+                Item.noMelee = true;
+                player.direction = (int)player.HorizontalDirectionTo(Main.MouseWorld);
+            }
+            else
+            {
+                Item.noMelee = false;
+                float prog = (float)Timer / (2 * player.itemAnimationMax / 3);
+                player.itemAnimation = (int)(player.itemAnimationMax * Math.Pow(MomentumProgress(prog), 2));
+            }
+
+        }
+        public override void ModifyHitNPC(Player player, NPC target, ref NPC.HitModifiers modifiers)
+        {
+            if (target.onFire || target.onFire2 || target.onFire3)
+                modifiers.FinalDamage *= 1.2f;
+        }
+        public static float MomentumProgress(float x)
+        {
+            return (x * x * 3) - (x * x * x * 2);
         }
         public override void OnHitNPC(Player player, NPC target, NPC.HitInfo hit, int damageDone)
         {
