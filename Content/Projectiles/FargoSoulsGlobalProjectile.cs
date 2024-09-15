b306994712fe6eb2044016a98eda955ab055b796
Jack <jacklukas@outlook.com>
2024-09-15 12:47:29 +0200

    Decreased damage of Nature Force shrooms

--- Content/Projectiles/FargoSoulsGlobalProjectile.cs
+++ Content/Projectiles/FargoSoulsGlobalProjectile.cs
@@ -1402,7 +1402,7 @@ namespace FargowiltasSouls.Content.Projectiles
 
             if (projectile.type == ProjectileID.CrystalLeafShot && player.HasEffect<NatureEffect>() && player.HasEffect<ShroomiteShroomEffect>())
             {
-                ShroomiteShroomEffect.SpawnShrooms(player, target, (int)(damageDone * 1.25f));
+                ShroomiteShroomEffect.SpawnShrooms(player, target, (int)(damageDone * 1f));
             }
         }
 
