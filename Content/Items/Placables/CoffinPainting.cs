debeac5501ad87fc2b56c310456c948854ac2e0f
gpuls <gavin.puls@icloud.com>
2024-09-27 15:17:16 -0400

    added coffin arena painting

---
+++ Content/Items/Placables/CoffinPainting.cs
@@ -0,0 +1,29 @@
+﻿using FargowiltasSouls.Content.Tiles;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace FargowiltasSouls.Content.Items.Placables
+{
+    public class CoffinPainting : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Terraria.GameContent.Creative.CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 50;
+            Item.height = 34;
+            Item.maxStack = 999;
+            Item.useTurn = true;
+            Item.autoReuse = true;
+            Item.useAnimation = 15;
+            Item.consumable = true;
+            Item.useTime = 10;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.rare = ItemRarityID.Blue;
+            Item.createTile = ModContent.TileType<CoffinPaintingSheet>();
+        }
+    }
+}
