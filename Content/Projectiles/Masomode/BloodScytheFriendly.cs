2c282e06fe368496916d02f2ff4aedcf20b3ac4d
Midnight295 <143055111+Midnight295@users.noreply.github.com>
2024-10-20 20:33:42 -0700

    fixed localization for chalice and rain enchant headers eye of cthulhu new scythes sprites and new scythe visuals made eye of cthulhu emit blood dust instead of vortex dust when recolors are turned off. queen bee now makes her roar sound instead of using soundid.roar spectral abominationn no longer makes the soundid.roar sound when summoned

--- Content/Projectiles/Masomode/BloodScytheFriendly.cs
+++ Content/Projectiles/Masomode/BloodScytheFriendly.cs
@@ -1,15 +1,50 @@
-﻿using Terraria.ID;
+﻿using FargowiltasSouls.Core.Systems;
+using FargowiltasSouls.Core;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using Luminance.Core.Graphics;
+using FargowiltasSouls.Assets.ExtraTextures;
 
 namespace FargowiltasSouls.Content.Projectiles.Masomode
 {
-    public class BloodScytheFriendly : ModProjectile
+    public class BloodScytheFriendly : ModProjectile//, IPixelatedPrimitiveRenderer
     {
+        public override string Texture => "FargowiltasSouls/Content/Projectiles/Masomode/BloodScytheVanilla1";
         public override void SetStaticDefaults()
         {
-            // DisplayName.SetDefault("Blood Scythe");
+            ProjectileID.Sets.TrailingMode[Type] = 1;
+            ProjectileID.Sets.TrailCacheLength[Type] = 20;
+        }
+        public override Color? GetAlpha(Color lightColor)
+        {
+            return Color.Red;
+        }
+        /*public float WidthFunction(float completionRatio)
+        {
+            float baseWidth = Projectile.scale * Projectile.width * 1.3f;
+            return MathHelper.SmoothStep(baseWidth, 3.5f, completionRatio);
         }
 
+        public static Color ColorFunction(float completionRatio)
+        {
+            return Color.Lerp(Color.Red, Color.Transparent, completionRatio) * 0.7f;
+        }
+
+        public void RenderPixelatedPrimitives(SpriteBatch spriteBatch)
+        {
+            ManagedShader shader = ShaderManager.GetShader("FargowiltasSouls.BlobTrail");
+            FargoSoulsUtil.SetTexture1(FargosTextureRegistry.FadedStreak.Value);
+            PrimitiveRenderer.RenderTrail(Projectile.oldPos, new(WidthFunction, ColorFunction, _ => Projectile.Size * 0.5f, Pixelate: true, Shader: shader), 25);
+            Texture2D glowTexture = ModContent.Request<Texture2D>("FargowiltasSouls/Content/Projectiles/GlowRing").Value;
+
+            Vector2 glowDrawPosition = Projectile.Center;
+
+            Main.EntitySpriteDraw(glowTexture, glowDrawPosition, null, Color.Teal, Projectile.rotation, glowTexture.Size() * 0.5f, Projectile.scale * 0.4f, SpriteEffects.None, 0);
+        }
+        */
         public override void SetDefaults()
         {
             Projectile.CloneDefaults(ProjectileID.DemonScythe);
