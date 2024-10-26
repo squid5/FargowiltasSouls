db9ce9accfeac56bd9ebe0385b2d5cbe1140baab
JavyzTaken <jacklukas@outlook.com>
2024-10-26 22:34:27 +0200

    Voltaic Chain: Use time 45 -> 35 -Lightning damage x1.5

--- Content/Projectiles/BossWeapons/ElectricWhipProj.cs
+++ Content/Projectiles/BossWeapons/ElectricWhipProj.cs
@@ -83,7 +83,7 @@ namespace FargowiltasSouls.Content.Projectiles.BossWeapons
                     {
                         SoundEngine.PlaySound(SoundID.DD2_LightningAuraZap, Projectile.Center);
                         if (FargoSoulsUtil.HostCheck)
-                            Projectile.NewProjectile(Projectile.InheritSource(Projectile), p.Center, p.DirectionTo(target.Center), ModContent.ProjectileType<ElectricWhipLightning>(), Projectile.originalDamage / 3, Projectile.knockBack, Projectile.owner, ai2: p.Distance(target.Center));
+                            Projectile.NewProjectile(Projectile.InheritSource(Projectile), p.Center, p.DirectionTo(target.Center), ModContent.ProjectileType<ElectricWhipLightning>(), Projectile.originalDamage / 2, Projectile.knockBack, Projectile.owner, ai2: p.Distance(target.Center));
                         Cooldowns[p.whoAmI] = /*Cooldowns[otherMinion.whoAmI] = */45;
                         LightningCounter++;
                         if (LightningCounter >= 4)
