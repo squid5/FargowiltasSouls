d336c8eb5cee6c7bfb329f91b518044681032170
JavyzTaken <jacklukas@outlook.com>
2024-10-27 02:12:44 +0200

    Dicer damage 60 -> 80

--- Content/Items/Weapons/BossDrops/Dicer.cs
+++ Content/Items/Weapons/BossDrops/Dicer.cs
@@ -35,7 +35,7 @@ namespace FargowiltasSouls.Content.Items.Weapons.BossDrops
             Item.useTime = 25;
             Item.shootSpeed = 16f;
             Item.knockBack = 2.5f;
-            Item.damage = 60;
+            Item.damage = 80;
             Item.value = Item.sellPrice(0, 10);
             Item.rare = ItemRarityID.Yellow;
         }
